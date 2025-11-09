044a196a286a07c2b3f55c92a81b2cb63b8d437a
Olalekan Yusuf <olasyusuf@gmail.com>
2025-11-09 05:36:59 +0100

    feat: update app join page (app/join.tsx)

---
+++ prodev-mobile-app-0x04/app/join.tsx
@@ -0,0 +1,102 @@
+import { Text, TextInput, View, TouchableOpacity, Image } from "react-native";
+import { styles } from "@/styles";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
+
+export default function Index() {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.navGroup}>
+          <Ionicons name="arrow-back" size={25} />
+          <Image source={require('@/assets/images/logo.png')} />
+        </View>
+        <Text style={styles.largeText}>Create your</Text>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <FontAwesome name="eye-slash" size={24} color="#7E7B7B" />
+          <Text style={styles.largeText}>Account</Text>
+        </View>
+        <Text style={styles.smallText}>
+          Enter your details to create a new account.
+        </Text>
+
+        <View style={styles.formGroup}>
+          <View>
+            <Text style={styles.placeholderText}>Email</Text>
+            <TextInput keyboardType="email-address" style={styles.inputField} />
+          </View>
+          <View style={{ marginTop: 20 }}>
+            <Text style={styles.placeholderText}>Password</Text>
+            <View style={styles.passwordGroup}>
+              <TextInput style={{ flex: 1 }} />
+            </View>
+          </View>
+          <View style={{ marginTop: 20 }}>
+            <Text style={styles.placeholderText}>Password</Text>
+            <View style={styles.passwordGroup}>
+              <TextInput style={{ flex: 1 }} />
+            </View>
+          </View>
+        </View>
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Create</Text>
+        </TouchableOpacity>
+
+        <View style={styles.dividerGroup}>
+          <View
+            style={styles.divider}
+          ></View>
+          <Text
+            style={styles.dividerText}
+          >
+            OR
+          </Text>
+          <View
+            style={styles.divider}
+          ></View>
+        </View>
+
+        <View style={styles.socialMediaButtonGroup}>
+          <TouchableOpacity style={styles.socialMediaButton}>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 5,
+              }}
+            >
+              <Image source={require("@/assets/images/google.png")} />
+              <Text style={styles.socialMediaButtonText}>
+                Continue with Google
+              </Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.socialMediaButton}>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 5,
+              }}
+            >
+              <Image source={require("@/assets/images/facebook.png")} />
+              <Text style={styles.socialMediaButtonText}>
+                Continue with Facebook
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.subTextGroup}>
+          <Text style={styles.subText}>Don't have an account?</Text>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <FontAwesome name="eye-slash" size={24} color="#7E7B7B" />
+            <Text style={styles.subTextJoin}>Join now</Text>
+          </View>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
